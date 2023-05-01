73733e67f2ac30a0cd3cb82ac3c981ced36cc895
stian13 <stian7062@gmail.com>
2023-05-01 09:47:42 -0500

    se realizo el sistema de inicio de sesion y un agregador de elementos a la base de datos

---
+++ main/conexion.php
@@ -0,0 +1,16 @@
+<?php
+
+$server = "localhost";
+$user = "sebas";
+$pass = "12345";
+$database = "phonereview";
+
+$conexion = new mysqli($server, $user, $pass, $database);
+
+if ($conexion->connect_errno){
+    die("conexion FALLIDA" . $conexion->connect_errno);
+}else{
+    echo "conectado";
+}
+
+?>
---
+++ main/maincode.js
@@ -0,0 +1,6 @@
+import {llamado} from './funciones.js';
+
+let experimento = document.querySelector('.li');
+experimento.addEventListener('click', function () {
+    llamado()
+})
